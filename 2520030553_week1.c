00191bfb483b87bf0418eec2a19329ae3dab731a
jaswanthbhaskar-elite <jaswanthbhaskar@klh.edu.in>
2026-08-22 15:10:45 +0530

    Refactor to execute commands via shell
    Updated the program to execute Linux commands using a shell.

--- 2520030553_week1.c
+++ 2520030553_week1.c
@@ -1,21 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<sys/wait.h>
-int main(){
-        char cmd[10];
-        pid_t pid;
-        printf("Enter command : \n");
-        scanf("%s",cmd);
-        pid=fork();
-        if(pid==0){
-                execlp("cmd","cmd",NULL);
-                printf("exec() system call failed");
-                exit(1);
-                }else{
-                        wait(NULL);
-                        printf("parent pid:%d",getppid());
-                        printf("child pid:%d",getpid());
-                }
-        return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    char command[100];
+    pid_t pid;
+
+    printf("Enter a Linux command: ");
+    fgets(command, sizeof(command), stdin);
+
+    pid = fork();
+
+    if (pid == 0) {
+        printf("Child PID: %d\n", getpid());
+
+        execl("/bin/sh", "sh", "-c", command, NULL);
+
+        exit(1);
+    }
+    else {
+        printf("Parent PID: %d\n", getpid());
+        printf("Child PID: %d\n", pid);
+
+        wait(NULL);
+
+        printf("Child process finished\n");
+    }
+
+    return 0;
 }
