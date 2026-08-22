e9583b97ebaebd8107e2f08c704fbfcba912907f
jaswanthbhaskar-elite <jaswanthbhaskar@klh.edu.in>
2026-08-22 15:05:57 +0530

    Update 2520030553_week2.c

--- 2520030553_week2.c
+++ 2520030553_week2.c
@@ -1,30 +1,20 @@
-#include <fcntl.h>   // for open()
-#include <unistd.h>  // for read(), write(), close()
-#include <stdio.h>   // for perror()
-#include <stdlib.h>  // for exit()
+#include <fcntl.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
-    int src, dst;
-    char buf[1024];
-    ssize_t n;
+int main() {
+    int source, destination;
+    char buffer[100];
+    int n;
 
-    if (argc != 3) {
-        write(2, "Usage: copy <source> <dest>\n", 28);
-        exit(1);
+    source = open("source.txt", O_RDONLY);
+    destination = open("destination.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+    while ((n = read(source, buffer, 100)) > 0) {
+        write(destination, buffer, n);
     }
 
-    src = open(argv[1], O_RDONLY);
-    if (src < 0) { perror("open source"); exit(1); }
+    close(source);
+    close(destination);
 
-    dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-    if (dst < 0) { perror("open dest"); close(src); exit(1); }
-
-    while ((n = read(src, buf, sizeof(buf))) > 0) {
-        if (write(dst, buf, n) != n) { perror("write"); exit(1); }
-    }
-    if (n < 0) perror("read");
-
-    close(src);
-    close(dst);
     return 0;
 }
